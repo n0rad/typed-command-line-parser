93aefc1b0a82b5f857a898d961d4c5e7d00e202b
Arnaud Lemaire <dev@norad.fr>
2012-04-18 15:44:27 +0200

    add cliparamdate

---
+++ src/main/java/net/awired/aclm/param/CliParamDate.java
@@ -0,0 +1,29 @@
+    private final class CliParamDate extends CliParam<Date> {
+
+        private SimpleDateFormat simpleDateFormat;
+        private final String format;
+
+        private CliParamDate(String name, String format) {
+            super(name);
+            this.format = format;
+            simpleDateFormat = new SimpleDateFormat(format);
+        }
+
+        @Override
+        public String getParamDescription() {
+            String res = "";
+            if (super.getParamDescription() != null) {
+                res = super.getParamDescription();
+            }
+            return res + " format: " + format;
+        }
+
+        @Override
+        public Date parse(String param) throws CliArgumentParseException {
+            try {
+                return simpleDateFormat.parse(param);
+            } catch (ParseException e) {
+                throw new CliArgumentParseException("unparsable date", e);
+            }
+        }
+    }
