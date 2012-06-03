6d3601b2412fe2a6714d6d072cd45d0047158a54
Arnaud Lemaire <dev@norad.fr>
2012-06-03 09:48:48 +0200

    add date param

--- src/main/java/net/awired/aclm/param/CliParamDate.java
+++ src/main/java/net/awired/aclm/param/CliParamDate.java
@@ -1,29 +1,36 @@
-    private final class CliParamDate extends CliParam<Date> {
+package net.awired.aclm.param;
 
-        private SimpleDateFormat simpleDateFormat;
-        private final String format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import net.awired.aclm.argument.CliArgumentParseException;
 
-        private CliParamDate(String name, String format) {
-            super(name);
-            this.format = format;
-            simpleDateFormat = new SimpleDateFormat(format);
+public class CliParamDate extends CliParam<Date> {
+
+    private SimpleDateFormat simpleDateFormat;
+    private final String     format;
+
+    private CliParamDate(String name, String format) {
+        super(name);
+        this.format = format;
+        simpleDateFormat = new SimpleDateFormat(format);
+    }
+
+    @Override
+    public String getParamDescription() {
+        String res = "";
+        if (super.getParamDescription() != null) {
+            res = super.getParamDescription();
         }
+        return res + " format: " + format;
+    }
 
-        @Override
-        public String getParamDescription() {
-            String res = "";
-            if (super.getParamDescription() != null) {
-                res = super.getParamDescription();
-            }
-            return res + " format: " + format;
-        }
-
-        @Override
-        public Date parse(String param) throws CliArgumentParseException {
-            try {
-                return simpleDateFormat.parse(param);
-            } catch (ParseException e) {
-                throw new CliArgumentParseException("unparsable date", e);
-            }
+    @Override
+    public Date parse(String param) throws CliArgumentParseException {
+        try {
+            return simpleDateFormat.parse(param);
+        } catch (ParseException e) {
+            throw new CliArgumentParseException("unparsable date", e);
         }
     }
+}
